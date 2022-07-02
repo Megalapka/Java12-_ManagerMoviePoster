1b737eecf30af70ab665d01d3a64fa6b565b9586
Megalapka <forvsykoe@list.ru>
2022-07-02 15:29:33 +0300

    Created PosterManager and PosterManagerTest

---
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -0,0 +1,41 @@
+package ru.netology.manager;
+
+public class PosterManager {
+    private String[] moviesList = new String[0];
+    private int limit;
+    public PosterManager() {
+        limit = 10;
+    }
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addMove(String movie) {
+        String[] tmp = new String[moviesList.length + 1];
+        for (int i = 0; i < moviesList.length; i++) {
+            tmp[i] = moviesList[i];
+        }
+        tmp[tmp.length-1] = movie;
+        moviesList = tmp;
+    }
+
+    public String[] findAll() {
+        return moviesList;
+    }
+
+    public String[] findLast() {
+        int resultLimit;
+        if (moviesList.length < limit) {
+            resultLimit = moviesList.length;
+        } else {
+            resultLimit = limit;
+        }
+
+        String[] tmp = new String[resultLimit];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = moviesList[moviesList.length - 1 - i];
+        }
+        return tmp;
+    }
+
+}
---
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -0,0 +1,62 @@
+package ru.netology.manager;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerTest {
+
+    @Test
+    public void shouldAddMovie() {
+        PosterManager manager  = new PosterManager(2);
+
+        manager.addMove("Film1");
+        manager.addMove("Film2");
+        manager.addMove("Film3");
+
+        String[] expected = {"Film1", "Film2", "Film3"};
+        String[] actual = manager.findAll();
+
+        assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldFindLastFive() {
+        PosterManager manager = new PosterManager(5);
+        manager.addMove("Film1");
+        manager.addMove("Film2");
+        manager.addMove("Film3");
+        manager.addMove("Film4");
+        manager.addMove("Film5");
+        manager.addMove("Film6");
+
+        String[] expected = {"Film6", "Film5", "Film4", "Film3", "Film2"};
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected,actual);
+
+    }
+
+    @Test
+    public void shouldFindLastTen() {
+        PosterManager manager = new PosterManager();
+        manager.addMove("Film1");
+        manager.addMove("Film2");
+        manager.addMove("Film3");
+        manager.addMove("Film4");
+        manager.addMove("Film5");
+        manager.addMove("Film6");
+        manager.addMove("Film7");
+        manager.addMove("Film8");
+        manager.addMove("Film9");
+        manager.addMove("Film10");
+        manager.addMove("Film11");
+        manager.addMove("Film12");
+
+        String[] expected = {"Film12", "Film11", "Film10", "Film9", "Film8", "Film7", "Film6", "Film5", "Film4", "Film3"};
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected,actual);
+
+    }
+}
